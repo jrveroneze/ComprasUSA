4a003a72bfaef028f10758681cb38bc850925f54
Junior Veroneze <oscarveronezejr@hotmail.com>
2019-05-20 15:55:50 -0300

    Insere layout, icones, imagens e launchscreen.

---
+++ ComprasUSA/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  ComprasUSA
+//
+//  Created by Oscar Veroneze Junior on 20/05/19.
+//  Copyright © 2019 Junior Veroneze. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
